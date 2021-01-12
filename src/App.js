d1289ef5fcab4082f92691cd7fafc25d60c867fb
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-12 16:32:46 +0100

    kekerino

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Scroll from './Scroll';
 // import {robots} from './robots';
 import './App.css';
 import ErrorBoundry from './ErrorBoundry';
+import { ThemeProvider } from 'styled-components';
+import { lightTheme, darkTheme } from './theme';
+import { GlobalStyles } from './global';
+import Toggle from './toggle.js'
+
 
 // const state ={
 // 	robots: robots,
@@ -19,6 +24,7 @@ function App (){
 	// 		searchfield: ''
 	// 	}
 	// }
+const [theme, setTheme] = useState('light');
 const [state, setState] = useState('initial')
 const [robots, setRobots] = useState([]);
 const [searchfield, setSearchfield] = useState('');
@@ -32,6 +38,17 @@ const [searchfield, setSearchfield] = useState('');
 	// 	// this.setState({ robots: robots});
 	// }
 
+
+// The function that toggles between themes
+const toggleTheme = () => {
+  // if the theme is not light, then set it to dark
+  if (theme === 'light') {
+    setTheme('dark');
+  // otherwise, it should be light
+  } else {
+    setTheme('light');
+  }
+}
 useEffect(() => { if (setState !== 'loaded') {	
 		fetch('https://jsonplaceholder.typicode.com/users')
 		.then(response => response.json())
@@ -60,26 +77,46 @@ if (state === 'initial')
  	{ 
  		// return <h1>Loading</h1>
  		return (
- 		<div className='tc'>
+ 		<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+ 		<GlobalStyles /> 
+        <div className='tc top:1rem'>
+       		
+        	<Toggle className='tc' theme={theme} toggleTheme={toggleTheme} />
 	 		<h1 className='f1'>RoboFriends</h1>
 			<SearchBox searchChange={onSearchChange}/>
 			<h1 className='tc'>Loading</h1> 
 		</div>
+        <footer>
+        </footer>
+    	</ThemeProvider>
 		)
  	}
 else if (state === 'loaded') {
 	if (filterRobotsName == '')
 	{
 		return(
-			<div className='tc'>	
+			<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+			<GlobalStyles />
+			
+			<div className='tc top:1rem'>	
+				
+				<Toggle className='tc' theme={theme} toggleTheme={toggleTheme} />
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={onSearchChange}/>
 				<p class='p1'>Search found no robots</p>
 			</div>
+	        <footer>
+	        </footer>
+
+	    	</ThemeProvider>
 			)
 	}else {		
 		return(
-			<div className='tc'>	
+			<ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+			<GlobalStyles />
+			<div className='tc top:1rem'>
+				
+				<Toggle className='tc' theme={theme} toggleTheme={toggleTheme} />
 				<h1 className='f1'>RoboFriends</h1>
 				<SearchBox searchChange={onSearchChange}/>
 					<ErrorBoundry>
@@ -87,6 +124,9 @@ else if (state === 'loaded') {
 					</ErrorBoundry>
 
 			</div>
+			 <footer>
+	        </footer>
+	    	</ThemeProvider>
 		);
 	}
 	}
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const lightTheme = {
+  body: 'linear-gradient(to left, rgba(7,27,82,1)0%, rgba(0,128,128,1) 100%)',
+  text: '#363537',
+  toggleBorder: '#FFF',
+  gradient: 'linear-gradient(#39598A, #79D7ED)',
+}
+
+export const darkTheme = {
+  body: 'linear-gradient(90deg, rgba(15,48,164,1) 5%, rgba(14,137,232,1) 40%, rgba(0,212,255,1) 100%);',
+  text: '#363537',
+  toggleBorder: '#6B8096',
+  gradient: 'linear-gradient(#091236, #1E215D)',
+}
