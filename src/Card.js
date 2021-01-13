cecefbb56aa2d370c45f184b966cd4abafc03f4a
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-13 20:15:05 +0100

    sf

--- src/Card.js
+++ src/Card.js
@@ -1,46 +1,46 @@
-import React, { useState, useEffect } from 'react';
+// import React, { useState, useEffect } from 'react';
 
 
-const Card = (props, theme) =>{
-	const { name, email, website, id } = props;
-	console.log(theme);
-	if (theme === 'light')
-	{
-	return (
-		<div class="myclass" className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
-			<div>
-				<h2 className='f5'>{name}</h2>
-				<p className='f6'>{email}</p>
-				<p2 className='f6'>{website}</p2>
-			</div>
-		</div>
-	)
-	}
-	else if (theme === 'dark') {
-		return (
-		<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
-			<div>
-				<h2 className='f5'>{name}</h2>
-				<p className='f6'>{email}</p>
-				<p2 className='f6'>{website}</p2>
-			</div>
-		</div>
-	)
-	}
-	else {
-		return (
-		<div className='tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5'>
-			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
-			<div>
-				<h2 className='f5'>{name}</h2>
-				<p className='f6'>{email}</p>
-				<p2 className='f6'>{website}</p2>
-			</div>
-		</div>
-	)
-	}
-}
+// const Card = (props) =>{
+// 	const { name, email, website, id } = props;
+// 	console.log(theme);
+// 	if (theme === 'light')
+// 	{
+// 	return (
+// 		<div class="myclass" className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+// 			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
+// 			<div>
+// 				<h2 className='f5'>{name}</h2>
+// 				<p className='f6'>{email}</p>
+// 				<p2 className='f6'>{website}</p2>
+// 			</div>
+// 		</div>
+// 	)
+// 	}
+// 	else if (theme === 'dark') {
+// 		return (
+// 		<div className='tc bg-light-blue dib br3 pa3 ma2 grow bw2 shadow-5'>
+// 			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
+// 			<div>
+// 				<h2 className='f5'>{name}</h2>
+// 				<p className='f6'>{email}</p>
+// 				<p2 className='f6'>{website}</p2>
+// 			</div>
+// 		</div>
+// 	)
+// 	}
+// 	else {
+// 		return (
+// 		<div className='tc bg-light-gray dib br3 pa3 ma2 grow bw2 shadow-5'>
+// 			<img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
+// 			<div>
+// 				<h2 className='f5'>{name}</h2>
+// 				<p className='f6'>{email}</p>
+// 				<p2 className='f6'>{website}</p2>
+// 			</div>
+// 		</div>
+// 	)
+// 	}
+// }
 
-export default Card;
+// export default Card;
