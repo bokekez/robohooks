0e2ccbe9c292fc5f43c31e39931127caf89b994e
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-15 11:41:30 +0100

    7

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,16 +1,33 @@
 import React from 'react';
 
-const SearchBox = ({ searchfield, searchChange}) => {
-	return (
+const SearchBox = ({ searchfield, searchChange, theme}) => {
+	console.log('+1');
+	if (theme === 'light') {
+
+		return (
 		<div className='tc pa2'>
 		<input
 		className ='pa3 ba b--green bg-lightest-blue'
 		type='search' placeholder='search robots' 
 		onChange={searchChange}
 		/>
-		<p class='tc pa1 f6 near-white'> search robots by name, email or website </p>
+		<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
 		</div>
 	);
+	}
+	else {
+		return (
+		<div className='tc pa2'>
+		<input
+		className ='pa3 ba b--green bg-lightest-blue'
+		type='search' placeholder='search robots' 
+		onChange={searchChange}
+		/>
+		<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
+		</div>
+		);
+	}
+	
 	
 }
 
--- src/theme.js
+++ src/theme.js
@@ -9,15 +9,15 @@ export const lightTheme = {
 export const darkTheme = {
   body: 'linear-gradient(90deg, rgba(15,48,164,1) 5%, rgba(14,137,232,1) 40%, rgba(0,212,255,1) 100%);',
   text: 'blue',
-  toggleBorder: '#6B8096',
-  gradient: 'linear-gradient(#091236, #1E215D)',
+  toggleBorder: '#FFF',
+  gradient: 'linear-gradient(90deg, rgba(110,175,214,1) 11%, rgba(91,127,254,1) 100%);',
 
 }
 
 export const grayTheme = {
   body: 'linear-gradient(90deg, rgba(77,73,74,1) 11%, rgba(162,164,172,1) 100%);',
   text: 'darkgray',
-  toggleBorder: '#6B8096',
+  toggleBorder: '#FFF',
   gradient: 'linear-gradient(90deg, rgba(77,73,74,1) 11%, rgba(135,161,146,1) 100%);',
 
 }
