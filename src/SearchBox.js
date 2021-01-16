19048fad760f121290fa1b482199d39fd564cdfa
shiny-one <69863141+shiny-one@users.noreply.github.com>
2021-01-16 18:40:05 +0100

    last

--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,22 +1,50 @@
 import React from 'react';
 
-const SearchBox = ({ searchfield, searchChange, theme}) => {
-	// let colorVar;
-	// swtich(props.theme) {
-	// 	case('light'):
-	// 		colorVar='bg-lightest-blue';
-	// 		break;
-	// 	case('dark'):
-	// 		colorVar='bg-dark-blue';
-	// 		break;
-	// 	case('gray'):
-	// 		colorVar='bg-gray';
-	// 		break;
-	// }
+const SearchBox = ({ searchfield, searchChange, theme }) => {
+	let colorVar;
+	switch(theme)
+	{
+		case 'light':
+			colorVar='bg-lightest-blue';
+			break;
+		case 'dark':
+			colorVar='bg-light-blue';
+			break;
+		case 'gray':
+			colorVar='bg-lightest-gray';
+			break;
+		default:
+			colorVar='bg-lightest-blue';
+	}
+		return (
+		<div className='tc pa2'>
+		<input className ={`pa3 ba b--green ${colorVar}`}
+		type='search' placeholder='search robots' 
+		onChange={searchChange} value={searchfield}
+		
+		/>
+		<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
+		</div>
+	);
+	
+	// else {
 	// 	return (
 	// 	<div className='tc pa2'>
 	// 	<input
-	// 	className = ´'pa3 ba b--green ${colorVar}'´
+	// 	className ='pa3 ba b--green bg-lightest-blue'
+	// 	type='search' placeholder='search robots' 
+	// 	onChange={searchChange} value={searchfield}
+	// 	/>
+	// 	<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
+	// 	</div>
+	// 	);
+	// }
+	// }
+	// if (theme === 'light') {
+	// return (
+	// 	<div className='tc pa2'>
+	// 	<input
+	// 	className = 'pa3 ba b--green bg-lightest-blue'
 	// 	type='search' placeholder='search robots' 
 	// 	onChange={searchChange} value={searchfield}
 	// 	/>
@@ -24,67 +52,31 @@ const SearchBox = ({ searchfield, searchChange, theme}) => {
 	// 	</div>
 	// );
 	// }
-	// else {
+	// else if (theme === 'dark') {
 	// 	return (
 	// 	<div className='tc pa2'>
 	// 	<input
-	// 	className ='pa3 ba b--green bg-lightest-blue'
+	// 	className = 'pa3 ba b--green bg-light-blue'
 	// 	type='search' placeholder='search robots' 
 	// 	onChange={searchChange} value={searchfield}
 	// 	/>
 	// 	<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
 	// 	</div>
-	// 	);
+	// );
 	// }
-	// }
-	if (theme === 'light') {
-	return (
-		<div className='tc pa2'>
-		<input
-		className = 'pa3 ba b--green bg-lightest-blue'
-		type='search' placeholder='search robots' 
-		onChange={searchChange} value={searchfield}
-		/>
-		<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
-		</div>
-	);
-	}
-	else if (theme === 'dark') {
-		return (
-		<div className='tc pa2'>
-		<input
-		className = 'pa3 ba b--green bg-light-blue'
-		type='search' placeholder='search robots' 
-		onChange={searchChange} value={searchfield}
-		/>
-		<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
-		</div>
-	);
-	}
-	else if (theme === 'gray') {
-		return (
-		<div className='tc pa2'>
-		<input
-		className = 'pa3 ba b--green bg-light-gray'
-		type='search' placeholder='search robots' 
-		onChange={searchChange} value={searchfield}
-		/>
-		<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
-		</div>
-	);
-	}
-	// else {
+	// else if (theme === 'gray') {
 	// 	return (
 	// 	<div className='tc pa2'>
 	// 	<input
-	// 	className ='pa3 ba b--green bg-lightest-blue'
+	// 	className = 'pa3 ba b--green bg-light-gray'
 	// 	type='search' placeholder='search robots' 
 	// 	onChange={searchChange} value={searchfield}
 	// 	/>
 	// 	<p class='tc pa1 f7 near-white'> search robots by name, email or website </p>
 	// 	</div>
-	// 	);
+	// );
 	// }
+
 }
 
 export default SearchBox;
